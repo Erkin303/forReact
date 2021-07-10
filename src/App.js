4242959240b2ac0df90fd165753404cf3d60b818
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-07-10 11:25:03 +0300

    git ga ulandi va  reactda ishlandi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          That's good
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container mt-4 mb-4">
+        <div className="d-flex justify-content-between">
+          <p className="fw-bold fs-3">Rober.co</p>
+          <ul>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#">Services</a></li>
+            <li> <a href="#">Aboute</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="container card2">
+        <div className="row">
+          <div className="col-5 offset-2">
+            <p className=" text-primary">Roberto Carlos</p>
+            <p className=" fs-1 fw-bold">Visual designer & Developer</p>
+            <p className="pin text-secondary">Hello I'm Roberto Carlos. I design interfaces - the metaphors that humanize them, and the systems that scale them. Let's create something together.</p>
+            <button className="btnp">HIRE ME</button>
+            <a href="#" className="ms-5">Download CV</a>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div >
   );
 }
 
